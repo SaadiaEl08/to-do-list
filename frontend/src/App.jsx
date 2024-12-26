fbb327e5a857cdea9a7608373d843f343f78ba4e
SaadiaEL08 <saadiaelachguir@gmail.com>
2024-12-26 18:32:35 +0100

    replace sidebar layout with intro page component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,17 @@
 import { Outlet } from 'react-router';
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/AppSidebar"
+import Intro from './pages/Intro';
 
 
 const App = () => {
+
     return (
-        <SidebarProvider>
-            <div>
-                <AppSidebar />
-                <main>
-                    <SidebarTrigger />
-                    <main>
-                        <Outlet />
-                    </main>
-                </main>
-            </div>
-        </SidebarProvider>
+        <div>
+            <Intro />
+            {/* <main className="h-full bg-[#1A202C]">
+                <Outlet />
+            </main> */}
 
-
+        </div>
     );
 };
 
