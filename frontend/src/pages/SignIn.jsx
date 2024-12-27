e3a5b9a6074c6b31d3bd63d55e12a7c8f4ca26ce
SaadiaEL08 <saadiaelachguir@gmail.com>
2024-12-27 12:14:26 +0100

    create sign in page component

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { ChevronLeft, EyeClosed } from "lucide-react";
+import { Link } from "react-router";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <main className="w-full h-screen bg-background text-foreground flex flex-col justify-evenly items-start p-4">
+      <div className="w-full">
+        <ChevronLeft />
+      </div>
+      <h1 className="text-3xl font-bold">Login</h1>
+      <div className="w-full flex flex-col gap-4 justify-start items-start">
+        <div className="w-full flex flex-col gap-2 justify-start items-start">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            placeholder="Enter your Username"
+            className="w-full p-2 placeholder:text-muted-foreground placeholder:opacity-50  border-2 border-muted-foreground bg-input rounded outline-none"
+          />
+        </div>
+        <div className="w-full flex flex-col gap-2 justify-start items-start">
+          <label htmlFor="password">Password</label>
+          <div className="w-full flex items-center">
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter your Password"
+              className="w-full h-full p-2 placeholder:text-muted-foreground placeholder:opacity-50  border-y-2 border-s-2 border-muted-foreground bg-input rounded-s outline-none"
+            />
+            <EyeClosed className="border-y-2 border-e-2 border-muted-foreground bg-input rounded-e w-12 h-full  text-center pe-3 " />
+            <img src="/fingerprint.svg" alt="fingerprint icon " className="w-12"/>
+          </div>
+        </div>
+      </div>
+      <button
+        className="w-full p-2 bg-primary rounded disabled:opacity-50 disabled:cursor-not-allowed"
+        disabled
+      >
+        Login
+      </button>
+      <div className="w-full h-fit flex items-center justify-center gap-1">
+        <div className="w-full border border-muted-foreground gap-2"></div>
+        <div className="pb-1 flex items-center justify-center text-muted-foreground">
+          or
+        </div>
+        <div className="w-full border border-muted-foreground"></div>
+      </div>
+      <div className="w-full flex flex-col gap-4 items-center">
+        <button className="w-full border border-primary h-12 rounded flex gap-1 items-center justify-center">
+          <img src="/google.svg" alt="google icon" /> Login with Google
+        </button>
+        <button className="w-full border border-primary h-12 rounded flex gap-1 items-center justify-center">
+          <img src="/apple.svg" alt="apple icon" /> Login with Appel
+        </button>
+      </div>
+      <div className="text-xs mx-auto">
+        <span className="text-muted-foreground">Don’t have an account?</span>{" "}
+        <Link to="/register">Register</Link>{" "}
+      </div>
+    </main>
+  );
+};
 
-export default SignIn
+export default SignIn;
