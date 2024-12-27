2d38ff7649103d53479faabd169752d010e09ec2
SaadiaEL08 <saadiaelachguir@gmail.com>
2024-12-27 22:03:57 +0100

    create home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
 
 const Home = () => {
+  const [tasks, setTasks] = useState([]);
   return (
-    <div>Home</div>
-  )
-}
+    <div className="">
+      {tasks.length === 0 ? (
+        <div className="w-full h-5/6  text-foreground flex flex-col justify-center items-center">
+          <img src="/homePic.svg" alt="home picture" />
+          <h1 className="text-2xl opacity-80">What do you want to do today?</h1>
+          <p className="opacity-80">Tap + to add your tasks</p>
+        </div>
+      ) : (
+        <p>tasks</p>
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
