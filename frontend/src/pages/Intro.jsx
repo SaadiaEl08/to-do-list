e909b998d0eee9ead5b64e668f2c0d763d801cbc
SaadiaEL08 <saadiaelachguir@gmail.com>
2024-12-27 22:04:13 +0100

    separate welcome from intro

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,11 +1,10 @@
 import Loading from "@/components/Loading";
 import { useEffect, useState } from "react";
-import { ChevronLeft} from "lucide-react";
-import { Link } from "react-router";
+import { useNavigate } from "react-router";
 
 const Intro = () => {
+  const navigate = useNavigate();
   const [showLoading, setShowLoading] = useState(true);
-  const [showWelcome, setShowWelcome] = useState(false);
   const [introPageIndex, setIntroPageIndex] = useState(1);
   const listOfIntroPagesData = [
     {
@@ -28,9 +27,9 @@ const Intro = () => {
     },
   ];
   const handleNextClick = () => {
-    introPageIndex < listOfIntroPagesData.length
-      ? setIntroPageIndex(introPageIndex + 1)
-      : setShowWelcome(true);
+    introPageIndex < listOfIntroPagesData.length ?
+      setIntroPageIndex(introPageIndex + 1)
+      : navigate("/welcome");
   };
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -45,93 +44,62 @@ const Intro = () => {
         <Loading />
       ) : (
         <main>
-          {!showWelcome &&
-            listOfIntroPagesData.map(
-              (page, index) =>
-                page.id === introPageIndex && (
-                  <div
-                    key={index}
-                    className="w-full h-screen flex flex-col justify-between items-center bg-background text-foreground p-4 pb-10"
-                  >
-                    <div className="w-full">
-                      <span
-                        className="text-muted-foreground"
-                        onClick={() => setShowWelcome(true)}
-                      >
-                        Skip
-                      </span>
-                    </div>
-                    <div className="w-full h-[50%] flex justify-center items-center">
-                      <img src={page.Image} />
-                    </div>
-                    <div className="step flex justify-evenly items-center w-[50%] mx-auto gap-4 ">
-                      {Array.from({ length: listOfIntroPagesData.length }).map(
-                        (_, index) => (
-                          <div
-                            key={index}
-                            className={`w-full border-2 ${
-                              index + 1 === page.id
-                                ? "border-foreground"
-                                : "border-muted-foreground "
-                            }`}
-                          ></div>
-                        )
-                      )}
-                    </div>
-                    <h1 className="title text-3xl font-bold">{page.title}</h1>
-                    <p className="text w-full text-center text-foreground opacity-80">
-                      {page.text}
-                    </p>
-                    <div className="w-full flex justify-between items-center gap-4">
-                      <button
-                        className="text-muted-foreground disabled:cursor-not-allowed"
-                        disabled={introPageIndex === 1}
-                        onClick={() => setIntroPageIndex(introPageIndex - 1)}
-                      >
-                        BACK
-                      </button>
-                      <button
-                        className="bg-primary px-6 py-3 rounded "
-                        onClick={handleNextClick}
-                      >
-                        {" "}
-                        {introPageIndex === listOfIntroPagesData.length
-                          ? "Get Started"
-                          : "Next"}
-                      </button>
-                    </div>
+          {listOfIntroPagesData.map(
+            (page, index) =>
+              page.id === introPageIndex && (
+                <div
+                  key={index}
+                  className="w-full min-h-screen h-screen flex flex-col justify-between items-center bg-background text-foreground p-4 pb-10"
+                >
+                  <div className="w-full">
+                    <span
+                      className="text-muted-foreground"
+                      onClick={() => navigate("/welcome")}
+                    >
+                      Skip
+                    </span>
                   </div>
-                )
-            )}
-          {showWelcome && (
-            <div className="w-full h-screen flex flex-col justify-start items-center bg-background text-foreground p-4 pb-10">
-              <div className="w-full">
-                <ChevronLeft onClick={() => setShowWelcome(false)} />
-              </div>
-              <div className="w-full h-full flex flex-col justify-between items-center">
-                <div className="w-full h-[50%] flex flex-col justify-center items-center gap-6">
-                  <h1 className="title text-3xl font-bold">
-                    Welcome to UpTodo
-                  </h1>
+                  <div className="w-full h-[50%] flex justify-center items-center">
+                    <img src={page.Image} />
+                  </div>
+                  <div className="step flex justify-evenly items-center w-[50%] mx-auto gap-4 ">
+                    {Array.from({ length: listOfIntroPagesData.length }).map(
+                      (_, index) => (
+                        <div
+                          key={index}
+                          className={`w-full border-2 ${
+                            index + 1 === page.id
+                              ? "border-foreground"
+                              : "border-muted-foreground "
+                          }`}
+                        ></div>
+                      )
+                    )}
+                  </div>
+                  <h1 className="title text-3xl font-bold">{page.title}</h1>
                   <p className="text w-full text-center text-foreground opacity-80">
-                    Please login to your account or create new account to
-                    continue
+                    {page.text}
                   </p>
-                </div>
-                <div className="w-full  flex flex-col justify-between items-center gap-4">
-                  <Link to="/login" className="w-full">
-                    <button className="w-full h-12 bg-primary px-6 py-3 rounded ">
-                      LOGIN
+                  <div className="w-full flex justify-between items-center gap-4">
+                    <button
+                      className="text-muted-foreground disabled:cursor-not-allowed"
+                      disabled={introPageIndex === 1}
+                      onClick={() => setIntroPageIndex(introPageIndex - 1)}
+                    >
+                      BACK
                     </button>
-                  </Link>
-                  <Link to="/register" className="w-full">
-                    <button className="w-full h-12 border-2 border-primary text-foreground">
-                      CREATE ACCOUNT
+                    <button
+                      className="bg-primary px-6 py-3 rounded "
+                      onClick={handleNextClick}
+                    >
+                      {" "}
+                      {introPageIndex === listOfIntroPagesData.length
+                        ? "Get Started"
+                        : "Next"}
                     </button>
-                  </Link>
+                  </div>
                 </div>
-              </div>
-            </div>
+              )
           )}
         </main>
       )}
