d0cd107c1da2e31c2eb0d77899b80d4bff1b12af
SaadiaEL08 <saadiaelachguir@gmail.com>
2024-12-27 21:58:21 +0100

    add routers

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,28 +1,31 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
-import {Home,SignIn,SignUp} from "./pages"
-const router = createBrowserRouter(
-    [
-        {
-            path: "/login",
-            element: <SignIn />
-        },
-        {
-            path: "/register",
-            element: <SignUp />
-        },
-        {
-            path: "/",
-            element: <App />,
-            children: [
-                { path: "", element: <Home /> },
-            ]
-        },
-        { path: "*", element: <h1>Not found</h1>}
-
-    ]
-
-
-);
+import { Home, SignIn, SignUp } from "./pages";
+import Intro from "./pages/Intro";
+import Welcome from "./pages/Welcome";
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <SignIn />,
+  },
+  {
+    path: "/register",
+    element: <SignUp />,
+  },
+  {
+    path: "/",
+    element: <Intro />,
+  },
+  {
+    path: "/welcome",
+    element: <Welcome />,
+  },
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "home", element: <Home /> }],
+  },
+  { path: "*", element: <h1>Not found</h1> },
+]);
 
 export default router;
