c3ce8c3ce54534fee186b5b5c05b8e71dafb9c0c
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-01 22:47:35 +0100

    add a function to next button for each step

--- frontend/src/components/ActionBar.jsx
+++ frontend/src/components/ActionBar.jsx
@@ -1,12 +1,23 @@
+import { steps } from "@/constants";
 import { useDispatch } from "react-redux";
 
-const ActionBar = () => {
+const ActionBar = ({ nextActionText = "Save", nextActionFunction ,nextClassName=""}) => {
   const dispatch = useDispatch();
   return (
     <div className="w-full flex items-center justify-between  ">
-      <button className="text-primary h-12 px-5 w-full" onClick={() => dispatch({ type: "SET_IS_OPEN_ADD_TASK", payload: false })}>Cancel</button>
-      <button className="text-foreground h-12 px-5 w-full bg-primary rounded ">
-        Save
+      <button
+        className="text-primary h-12 px-5 w-full"
+        onClick={() =>
+          dispatch({ type: "SET_STEP", payload: steps.TASK_FORM })
+        }
+      >
+        Cancel
+      </button>
+      <button
+        className={"text-foreground h-12 px-5 w-full bg-primary rounded " + nextClassName}
+        onClick={nextActionFunction}
+      >
+        {nextActionText}
       </button>
     </div>
   );
