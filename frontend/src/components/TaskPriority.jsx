5db76da0b255779b23e57d83f2f4aa86f505dee9
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-01 23:34:47 +0100

    update taskPriority component

--- frontend/src/components/TaskPriority.jsx
+++ frontend/src/components/TaskPriority.jsx
@@ -1,40 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ActionBar from "./ActionBar";
+import { useDispatch, useSelector } from "react-redux";
+import { priorities } from "@/constants";
 
 const TaskPriority = () => {
-  const [selectedPriority, setSelectedPriority] = useState(null);
+  const dispatch = useDispatch();
+  const taskPriority = useSelector((state) => state.taskInfo.priority);
+  const [selectedPriority, setSelectedPriority] = useState(taskPriority);
 
-  const priority = [
-    {
-      name: "High",
-      color: "#f43f5e",
-    },
-    {
-      name: "Medium",
-      color: "#f59e0b",
-    },
-    {
-      name: "Low",
-      color: "#3b82f6",
-    },
-  ];
+  useEffect(() => {
+    if (selectedPriority) {
+      dispatch({
+        type: "UPDATE_TASK_INFO",
+        payload: {
+          priority: selectedPriority,
+        },
+      });
+    }
+  }, [selectedPriority, dispatch]);
+
   return (
     <div className=" w-[80vw] ">
       <h1 className="text-xl font-bold">Task Priority</h1>
       <div className="flex gap-4 p-2  ">
-        {priority.map((item) => (
+        {priorities.map((item) => (
           <div
             key={item.name}
-            className={`transition-all duration-300 rounded flex flex-col gap-2 items-center justify-center  aspect-square w-1/3 ${selectedPriority === item.name ? "bg-primary" : ""}`}
-            onClick={() => setSelectedPriority(item.name)}
+            className={`transition-all duration-300 rounded flex flex-col gap-2 items-center justify-center  aspect-square w-1/3 ${
+              selectedPriority === item.name ? "bg-primary" : ""
+            }`}
+            onClick={() => {
+              setSelectedPriority(item.name);
+            }}
           >
-            <div className="w-5 h-5 rounded-full" style={{ backgroundColor: item.color }}></div>
+            <div
+              className="w-5 h-5 rounded-full"
+              style={{ backgroundColor: item.color }}
+            ></div>
             <p>{item.name}</p>
           </div>
         ))}
       </div>
-      <ActionBar/>
-
+      <ActionBar />
     </div>
   );
 };
