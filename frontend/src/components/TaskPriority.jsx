f6b597c0ecb2352ab6f59a2ced83435ddd56849c
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-02 17:28:33 +0100

    save infos only when click on save btn

--- frontend/src/components/TaskPriority.jsx
+++ frontend/src/components/TaskPriority.jsx
@@ -1,24 +1,12 @@
 import { useEffect, useState } from "react";
 import ActionBar from "./ActionBar";
 import { useDispatch, useSelector } from "react-redux";
-import { priorities } from "@/constants";
+import { priorities, steps } from "@/constants";
 
 const TaskPriority = () => {
   const dispatch = useDispatch();
   const taskPriority = useSelector((state) => state.taskInfo.priority);
   const [selectedPriority, setSelectedPriority] = useState(taskPriority);
-
-  useEffect(() => {
-    if (selectedPriority) {
-      dispatch({
-        type: "UPDATE_TASK_INFO",
-        payload: {
-          priority: selectedPriority,
-        },
-      });
-    }
-  }, [selectedPriority, dispatch]);
-
   return (
     <div className=" w-[80vw] ">
       <h1 className="text-xl font-bold">Task Priority</h1>
@@ -29,9 +17,11 @@ const TaskPriority = () => {
             className={`transition-all duration-300 rounded flex flex-col gap-2 items-center justify-center  aspect-square w-1/3 ${
               selectedPriority === item.name ? "bg-primary" : ""
             }`}
-            onClick={() => {
-              setSelectedPriority(item.name);
-            }}
+            onClick={() =>
+              selectedPriority === item.name
+                ? setSelectedPriority("")
+                : setSelectedPriority(item.name)
+            }
           >
             <div
               className="w-5 h-5 rounded-full"
@@ -41,7 +31,18 @@ const TaskPriority = () => {
           </div>
         ))}
       </div>
-      <ActionBar />
+      <ActionBar
+        nextActionFunction={() => {
+          dispatch({
+            type: "UPDATE_TASK_INFO",
+            payload: {
+              priority: selectedPriority,
+            },
+          });
+          dispatch({ type: "SET_STEP", payload: steps.TASK_FORM });
+        }}
+        
+      />
     </div>
   );
 };
