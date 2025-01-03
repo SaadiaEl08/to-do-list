f31819e427e8482cf6f254a4b7f4358a43593b8a
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-03 16:18:50 +0100

    add formatted title

--- frontend/src/components/TaskPreview.jsx
+++ frontend/src/components/TaskPreview.jsx
@@ -1,14 +1,15 @@
 import { categories, priorities } from "@/constants";
 import dayjs from "dayjs";
-import { Eye, Flag, FlagIcon } from "lucide-react";
+import { Eye, Flag } from "lucide-react";
 import { cloneElement } from "react";
 
 const TaskPreview = ({ task }) => {
-  console.log(task);
   const { title, date, time, priority, category } = task;
   // Get category and priority info
   const categoryInfo = categories.find((item) => item.name === category) || {};
   const priorityInfo = priorities.find((item) => item.name === priority) || {};
+  // formate text
+  const formattedTitle = title.length > 30 ? title.slice(0, 30) + "..." : title;
 
   // Format time and date
   const formattedTime = time.format("HH:mm");
@@ -17,11 +18,12 @@ const TaskPreview = ({ task }) => {
     if (date === dayjs().add(1, "day")) return "Tomorrow";
     return date.format("dddd, MMM DD");
   };
+
   return (
-    <div className="w-full text-foreground flex items-center justify-between bg-dropDown rounded">
+    <div className="w-full text-foreground flex items-center justify-between bg-dropDown rounded-lg">
       <Eye className="w-10" />
       <div className=" w-full flex flex-col p-2 gap-3">
-        <h1 className="opacity-90">{title}</h1>
+        <h1 className="opacity-90">{formattedTitle}</h1>
         <div className=" w-full flex items-center justify-between gap-2 ">
           <div className="flex flex-wrap gap-1 w-full text-muted-foreground text-xs ">
             <p>{getDate(date)}</p>
@@ -31,16 +33,20 @@ const TaskPreview = ({ task }) => {
           <div className="flex items-center justify-end gap-2">
             <div
               className={"flex items-center gap-2 rounded px-3 py-2 text-sm "}
-              style={{ backgroundColor: categoryInfo.color }}
+              style={{
+                backgroundColor: categoryInfo.color + "99",
+              }}
             >
               {cloneElement(categoryInfo.icon, {
-                className: "w-4 h-4",
+                className: "w-4 h-4 font-bold ",
+                stroke: categoryInfo.color,
+                strokeWidth: 3,
               })}
               {categoryInfo.name}
             </div>
             <div
               className={"flex items-center gap-2 rounded px-3 py-2 text-sm "}
-              style={{ backgroundColor: priorityInfo.color }}
+              style={{ backgroundColor: priorityInfo.color + "99" }}
             >
               <Flag className="w-4 h-4" />
               {priorityInfo.name}
