87d2cf259f76d10882d172c22aaebe961f9a8f46
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-04 21:35:23 +0100

    install framer motion

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -1,12 +1,20 @@
+import { motion } from "motion/react";
 const Loading = ({ className, ...props }) => {
   return (
-    <div className={`w-full min-h-screen h-screen flex flex-col gap-2 justify-center items-center bg-background text-foreground ` + className} {...props}>
-      <div className="logo">
+    <div
+      className={
+        `fixed top-0 left-0  w-full min-h-screen h-screen flex flex-col gap-2 justify-center items-center bg-background text-foreground ` +
+        className
+      }
+      {...props}
+    >
+      <div
+        className="logo"
+        
+      >
         <img src="/logo.svg" />
       </div>
-      <span className="text-4xl font-bold">
-        UpTodo
-      </span>
+      <span className="text-4xl font-bold">UpTodo</span>
     </div>
   );
 };
