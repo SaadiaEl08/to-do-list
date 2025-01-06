25d71a278ea45c95fb10168be7c8555cdf6585d5
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-06 01:11:29 +0100

    update tasks  in app component all components can access em when ever and wherever

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,83 @@ import { Outlet } from "react-router";
 import NavigationMenu from "./components/NavigationMenu";
 import TopSection from "./components/TopSection";
 import TaskAction from "./components/TaskAction";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import dayjs from "dayjs";
 
 const App = () => {
   const isOpenAddTask = useSelector((state) => state.isOpenAddTask);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const tasksFromApi = [
+      {
+        id: 1,
+        title:
+          "Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1",
+        description:
+          "Description 1 Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1Task 1",
+        date: dayjs("2024-12-31"),
+        time: dayjs().add(1, "hour"),
+        priority: "High",
+        category: "Work",
+        isCompleted: false,
+      },
+      {
+        id: 2,
+        title: "Task 2",
+        description: "Description 2",
+        date: dayjs().add(1, "day"),
+        time: dayjs().add(1, "hour"),
+        priority: "Medium",
+        category: "Home",
+        isCompleted: false,
+      },
+      {
+        id: 3,
+        title: "Task 3",
+        description: "Description 3",
+        date: dayjs().add(1, "day"),
+        time: dayjs().add(1, "hour"),
+        priority: "Low",
+        category: "Grocery",
+        isCompleted: true,
+      },
+      {
+        id: 4,
+        title: "Task 3",
+        description: "Description 3",
+        date: dayjs().add(1, "day"),
+        time: dayjs().add(1, "hour"),
+        priority: "Low",
+        category: "Grocery",
+        isCompleted: true,
+      },
+      {
+        id: 5,
+        title: "Task 3",
+        description: "Description 3",
+        date: dayjs().add(1, "day"),
+        time: dayjs().add(1, "hour"),
+        priority: "Low",
+        category: "Grocery",
+        isCompleted: true,
+      },
+      {
+        id: 6,
+        title: "Task 3",
+        description: "Description 3",
+        date: dayjs().add(1, "day"),
+        time: dayjs().add(1, "hour"),
+        priority: "Low",
+        category: "Grocery",
+        isCompleted: true,
+      },
+    ];
+    dispatch({ type: "SET_TASKS", payload: tasksFromApi });
+  }, [dispatch]);
   return (
-    <main className="w-full min-h-screen h-screen bg-background flex flex-col justify-end">
-      <div className="h-full">
+    <main className="w-full  h-screen bg-background flex flex-col justify-end">
+      <div className="h-full overflow-scroll">
         <TopSection />
         <Outlet />
       </div>
