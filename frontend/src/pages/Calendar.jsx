6c406368683914cb6d41a533dd88eb9346f30c0e
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-06 01:13:42 +0100

    complete calendar page

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,11 +1,34 @@
 import CalendarComponent from "@/components/CalendarComponent";
-import { useSelect } from "@mui/base";
+import TaskPreview from "@/components/TaskPreview";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 const Calendar = () => {
-  const tasks = useSelect((state) => state.tasks);
+  const tasks = useSelector((state) => state.tasks);
+  const [selectedDay, setSelectedDay] = useState(dayjs());
+  const [filteredTasks, setFilteredTasks] = useState([]);
+
+  useEffect(() => {
+    const filtered = tasks.filter((task) =>
+      task.date.isSame(selectedDay, "day")
+    );
+    setFilteredTasks(filtered);
+  }, [selectedDay, tasks]);
+
   return (
-    <div className="border ">
-      <CalendarComponent tasks={tasks} />
+    <div className="p-4 flex flex-col gap-4">
+      <CalendarComponent
+        selectedDay={selectedDay}
+        setSelectedDay={setSelectedDay}
+      />
+      <div className="flex flex-col gap-4">
+        {filteredTasks.length > 0 ? (
+          filteredTasks.map((task) => <TaskPreview key={task.id} task={task} />)
+        ) : (
+          <p className="text-foreground text-center ">No tasks for today</p>
+        )}
+      </div>
     </div>
   );
 };
