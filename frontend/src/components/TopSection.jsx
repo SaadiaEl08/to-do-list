04e310a816f3f0de76a4648381f78c64995427eb
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-07 23:47:14 +0100

    update TopSection component to display the current route title dynamically

--- frontend/src/components/TopSection.jsx
+++ frontend/src/components/TopSection.jsx
@@ -1,9 +1,12 @@
 import { ListFilter } from "lucide-react";
+import { useLocation } from "react-router";
 
 const TopSection = () => {
+  const pathname = useLocation().pathname;
+  const title=pathname.split("/")[1];
   return <div className="w-full text-foreground flex items-center justify-between px-6 py-4">
     <ListFilter />
-    <p>Home</p>
+    <p className="capitalize">{title}</p>
     <img src="/logo.svg" alt="profile image" className="rounded-full w-11 h-11 border" />
 
 
