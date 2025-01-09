06732e7b676ca0c035b461b7289779dbc0c6ad7b
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-09 23:52:53 +0100

    add cancel action functionality to ActionBar component

--- frontend/src/components/ActionBar.jsx
+++ frontend/src/components/ActionBar.jsx
@@ -1,21 +1,34 @@
 import { steps } from "@/constants";
 import { useDispatch } from "react-redux";
 
-const ActionBar = ({ nextActionText = "Save", nextActionFunction ,nextClassName=""}) => {
+const ActionBar = ({
+  nextActionText = "Save",
+  nextActionFunction,
+  cancelActionFunction,
+  nextClassName = "",
+}) => {
   const dispatch = useDispatch();
   return (
     <div className="w-full flex items-center justify-between  ">
       <button
         className="text-primary h-12 px-5 w-full"
-        onClick={() =>
-          dispatch({ type: "SET_STEP", payload: steps.TASK_FORM })
+        onClick={
+          cancelActionFunction
+            ? cancelActionFunction
+            : () => dispatch({ type: "SET_STEP", payload: steps.TASK_FORM })
         }
       >
         Cancel
       </button>
       <button
-        className={"text-foreground h-12 px-5 w-full bg-primary rounded " + nextClassName}
-        onClick={nextActionFunction?nextActionFunction :() => dispatch({ type: "SET_STEP", payload: steps.TASK_FORM })}
+        className={
+          "text-foreground h-12 px-5 w-full bg-primary rounded " + nextClassName
+        }
+        onClick={
+          nextActionFunction
+            ? nextActionFunction
+            : () => dispatch({ type: "SET_STEP", payload: steps.TASK_FORM })
+        }
       >
         {nextActionText}
       </button>
