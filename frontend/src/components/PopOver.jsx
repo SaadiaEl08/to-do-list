f5968aad1337366b598a213160140ea5ad000b04
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-09 23:53:58 +0100

    add hide-scrollbar class to PopOver component for improved styling

--- frontend/src/components/PopOver.jsx
+++ frontend/src/components/PopOver.jsx
@@ -1,13 +1,13 @@
 const PopOver = ({ isOpen, toggle, children }) => {
   return (
     <div
-      className={`fixed text-foreground top-0 left-0 w-full min-h-full bg-black bg-opacity-50 z-30 ${
+      className={`fixed text-foreground top-0 left-0 w-full min-h-full bg-black bg-opacity-50 z-30  ${
         isOpen ? "block" : "hidden"
       }`}
-      onClick={toggle}
+      onClick={toggle} 
     >
       <div
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-popover rounded-xl  p-4 z-20 max-h-[90vh]  overflow-y-auto  border"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-popover rounded-xl  p-4 z-20 max-h-[90vh]  overflow-y-auto  border  hide-scrollbar"
         onClick={(e) => e.stopPropagation()} 
       >
         {children}
