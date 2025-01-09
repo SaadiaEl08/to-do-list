791fad467909e409521aadf5463e48da74b4584e
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-09 23:53:13 +0100

    add ChangeAccountImage component for account image management

---
+++ frontend/src/components/ChangeAccountImage.jsx
@@ -0,0 +1,48 @@
+import ActionBar from "./ActionBar";
+import PopOver from "./PopOver";
+
+const ChangeAccountImage = ({ setChangeAccount }) => {
+  const importFromGallery = () => {};
+  const takePicture = () => {};
+  const importFromGoogleDrive = () => {};
+  const list = [
+    {
+      title: "Tack picture",
+      onClick: takePicture,
+    },
+    {
+      title: "Import from gallery",
+      onClick: importFromGallery,
+    },
+    {
+      title: "Import from Google Drive",
+      onClick: importFromGoogleDrive,
+    },
+  ];
+  return (
+    <PopOver isOpen={true} toggle={() => setChangeAccount(null)}>
+      <div className="flex flex-col items-center gap-4 p-4 w-[80vw]">
+        <h3>Change account name</h3>
+        <div className="w-full border "></div>
+        <div className="w-full flex flex-col gap-4 ">
+          {list.map((item) => (
+            <div
+              className="flex items-center gap-2  hover:bg-slate-600 cursor-pointer p-3 "
+              key={item.title}
+              onClick={item.onClick}
+            >
+              <p>{item.title}</p>
+            </div>
+          ))}
+        </div>
+
+        <ActionBar
+          cancelActionFunction={() => setChangeAccount(null)}
+          nextActionFunction={() => setChangeAccount(null)}
+        />
+      </div>
+    </PopOver>
+  );
+};
+
+export default ChangeAccountImage;
