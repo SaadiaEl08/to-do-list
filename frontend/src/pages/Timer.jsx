f609ec6e445f383f48e3f51e2fe75f0a28bd7d6f
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-10 18:40:19 +0100

    refactor(Timer): integrate SmallClocks component and enhance audio playback logic

--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 import BigTimerClock from "@/components/BigTimerClock";
 import SelectTime from "@/components/SelectTime";
 import { Pause, Play, RotateCcw } from "lucide-react";
+import SmallClocks from "@/components/SmallClocks";
 
 const Timer = () => {
   const [isCounting, setIsCounting] = useState(false);
@@ -12,12 +13,7 @@ const Timer = () => {
   });
 
   const intervalRef = useRef(null); // Ref to store the interval ID
-
-  const proposalsTimers = [
-    { id: 1, time: "01:00" },
-    { id: 2, time: "05:00" },
-    { id: 3, time: "15:00" },
-  ];
+  const audio = new Audio("/clockSoundEffect.mp3");
 
   const scrollToTime = (unit, value) => {
     const container = document.querySelector(`.time-${unit}-container`);
@@ -31,15 +27,8 @@ const Timer = () => {
     }
   };
 
-  const handleMiniClockClick = (time) => {
-    const [hours, minutes] = time.split(":").map(Number);
-    setSelectedTime({ hours, minutes, seconds: 0 });
-    scrollToTime("hours", hours);
-    scrollToTime("minutes", minutes);
-    scrollToTime("seconds", 0); // Default to 0 seconds
-  };
-
   const handleResetTime = () => {
+    audio.pause();
     setSelectedTime({ hours: 0, minutes: 0, seconds: 0 });
     scrollToTime("hours", 0);
     scrollToTime("minutes", 0);
@@ -53,8 +42,13 @@ const Timer = () => {
 
   const startCounting = () => {
     setIsCounting(true);
-    if (intervalRef.current) return; 
+    if (intervalRef.current) return;
     intervalRef.current = setInterval(() => {
+      audio.loop = false; // Ensure it doesn't loop automatically
+      // Play sound for 1.5 second
+      audio.pause();
+      audio.currentTime = 1.5;
+      audio.play();
       setSelectedTime((prevTime) => {
         const { hours, minutes, seconds } = prevTime;
         if (seconds > 0) {
@@ -64,6 +58,8 @@ const Timer = () => {
         } else if (hours > 0) {
           return { hours: hours - 1, minutes: 59, seconds: 59 };
         } else {
+          audio.pause();
+          audio.currentTime = 0;
           clearInterval(intervalRef.current); // Stop the interval when the timer reaches zero
           intervalRef.current = null;
           setIsCounting(false);
@@ -74,6 +70,11 @@ const Timer = () => {
   };
 
   const stopCounting = () => {
+    if (audio) {
+      audio.pause(); // Pause the audio
+      console.log("Audio paused:", audio.paused); // Check if audio is paused
+      console.log("Audio currentTime:", audio.currentTime); // Check playback position
+    }
     setIsCounting(false);
     if (intervalRef.current) {
       clearInterval(intervalRef.current); // Stop the interval
@@ -84,19 +85,8 @@ const Timer = () => {
   return (
     <div className="container flex flex-col justify-center items-center pt-3 gap-4">
       <BigTimerClock selectedTime={selectedTime} />
-
       {/* Mini Clocks */}
-      <div className="proposals-timers w-full flex justify-center items-center gap-4">
-        {proposalsTimers.map((item) => (
-          <div
-            key={item.id}
-            className="time-circle flex flex-col justify-center items-center border-4 border-slate-600 bg-slate-800 rounded-full w-1/5 aspect-square cursor-pointer"
-            onClick={() => handleMiniClockClick(item.time)}
-          >
-            <span className="text-xl text-foreground">{item.time}</span>
-          </div>
-        ))}
-      </div>
+      <SmallClocks setSelectedTime={setSelectedTime} scrollToTime={scrollToTime} />
       {!isCounting && (
         <SelectTime
           selectedTime={selectedTime}
