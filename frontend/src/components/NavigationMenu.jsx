d00785237ad62a9f0cbcd0bf0f7f390c45b3f86a
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-12 22:31:47 +0100

    refactor(NavigationMenu): simplify navigation item rendering with a new Li component for better readability and maintainability

--- frontend/src/components/NavigationMenu.jsx
+++ frontend/src/components/NavigationMenu.jsx
@@ -2,6 +2,7 @@ import { CalendarDays, Clock, Home, Plus, User } from "lucide-react";
 import { Link, useLocation } from "react-router";
 import { useDispatch } from "react-redux";
 import { steps } from "@/constants";
+import { cloneElement } from "react";
 
 const NavigationMenu = () => {
   const currentPath = useLocation().pathname;
@@ -20,40 +21,22 @@ const NavigationMenu = () => {
   return (
     <div className="relative text-foreground w-full">
       <nav className="w-full bg-muted list-none flex items-center justify-between">
-        <ul className="flex items-center justify-evenly  w-2/5 ">
-          {navigationItems.slice(0, 2).map((item) => (
-            <li key={item.id} className={` p-2 rounded `}>
-              <Link
-                to={`${item.link}`}
-                className={`flex flex-col items-center justify-evenly ${
-                  currentPath === item.link ? "border-b-2  border-primary text-primary" : ""
-                }`}
-              >
-                {item.icon}
-                <span className="opacity-80">{item.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <ul className="flex items-center justify-evenly  w-2/5 ">
-          {navigationItems.slice(2, 4).map((item) => (
-            <li key={item.id} className={` p-2 rounded `}>
-              <Link
-                to={`${item.link}`}
-                className={`flex flex-col items-center justify-evenly ${
-                  currentPath === item.link ? "border-b-2  border-primary text-primary" : ""
-                }`}
-              >
-                {item.icon}
-                <span className="opacity-80">{item.name}</span>
-              </Link>
-            </li>
-          ))}
+        <ul className="flex items-center justify-between w-full">
+          <div className="flex items-center justify-evenly  w-2/4   ">
+            {navigationItems.slice(0, 2).map((item) => (
+              <Li key={item.id} item={item} currentPath={currentPath} />
+            ))}
+          </div>
+          <div className="flex items-center justify-evenly  w-2/4 ">
+            {navigationItems.slice(2, 4).map((item) => (
+              <Li key={item.id} item={item} currentPath={currentPath} />
+            ))}
+          </div>
         </ul>
       </nav>
-      <div className="rounded-full absolute bg-primary w-16 h-16 flex items-center justify-center -top-8 left-1/2 -translate-x-1/2 ">
+      <div className="rounded-full absolute bg-primary w-16 h-16 flex items-center justify-center -top-8 left-1/2 -translate-x-1/2  md:w-20 md:h-20">
         <Plus
-          className="w-8 h-8 cursor-pointer"
+          className="w-8 h-8 cursor-pointer md:w-12 md:h-12"
           onClick={() => {
             handleAddTaskClick();
           }}
@@ -62,5 +45,23 @@ const NavigationMenu = () => {
     </div>
   );
 };
-
 export default NavigationMenu;
+const Li = ({ item, currentPath }) => {
+  return (
+    <li className={` p-2 rounded  md:text-2xl`}>
+      <Link
+        to={`${item.link}`}
+        className={`flex flex-col items-center justify-evenly ${
+          currentPath === item.link
+            ? "border-b-2  border-primary text-primary"
+            : ""
+        }`}
+      >
+        {cloneElement(item.icon, {
+          className: "md:w-8 md:h-8",
+        })}
+        <span className="opacity-80">{item.name}</span>
+      </Link>
+    </li>
+  );
+};
