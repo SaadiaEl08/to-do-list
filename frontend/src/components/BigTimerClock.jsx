b74053c2f242d463cffbb3c65e14a0471c6c3784
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-12 22:30:25 +0100

    refactor(BigTimerClock): adjust font size for improved readability on larger screens

--- frontend/src/components/BigTimerClock.jsx
+++ frontend/src/components/BigTimerClock.jsx
@@ -1,7 +1,7 @@
 const BigTimerClock = ({selectedTime}) => {
   return (
     <div className="time-circle flex flex-col justify-center items-center border-[20px] border-slate-600 bg-primary rounded-full w-3/5 aspect-square">
-      <span className="text-4xl text-foreground">
+      <span className="text-4xl text-foreground md:text-6xl ">
         {`${String(selectedTime.hours).padStart(2, "0")}:${String(
           selectedTime.minutes
         ).padStart(2, "0")}:${String(selectedTime.seconds).padStart(2, "0")}`}
