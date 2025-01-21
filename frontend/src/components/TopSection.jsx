0b4956131dcde775a592e956fef0e0eca2a9b4ca
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-21 23:50:14 +0100

    feat(TopSection): update profile image source to use Redux state

--- frontend/src/components/TopSection.jsx
+++ frontend/src/components/TopSection.jsx
@@ -1,12 +1,15 @@
 import { ThemeContext } from "@/contexts/ThemeContext";
 import { Moon, Sun } from "lucide-react";
 import { useContext } from "react";
+import { useSelector } from "react-redux";
 import { useLocation } from "react-router";
 
 const TopSection = () => {
   const pathname = useLocation().pathname;
   const { theme, toggleTheme } = useContext(ThemeContext);
   const title = pathname.split("/")[1];
+  const { image } = useSelector((state) => state.accountInfo);
+
   return (
     <div className="w-full text-foreground flex items-center justify-between px-6 py-4">
       <p className="capitalize text-xl md:text-2xl">{title}</p>
@@ -14,14 +17,14 @@ const TopSection = () => {
         {theme === "dark" ? (
           <Sun className="w-6 h-6 cursor-pointer" onClick={toggleTheme} />
         ) : (
-          <Moon className="w-6 h-6 cursor-pointer"  onClick={toggleTheme} />
+          <Moon className="w-6 h-6 cursor-pointer" onClick={toggleTheme} />
         )}
-
         <img
-          src="/logo.svg"
-          alt="profile image"
-          className="rounded-full w-11 h-11 border"
+          src={image}
+          alt="profile photo"
+          className="rounded-full w-11 h-11 object-fill -scale-x-100 border border-primary"
         />
+        
       </div>
     </div>
   );
