c800b9b74fc59ca301b9fc31fefb3007b11d8d46
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-23 00:16:19 +0100

    feat(PopOver): integrate keyboard detection for dynamic positioning

--- frontend/src/components/PopOver.jsx
+++ frontend/src/components/PopOver.jsx
@@ -1,14 +1,20 @@
+import useKeyboardDetection from "@/hooks/useKeyboardDetection";
+
 const PopOver = ({ isOpen, toggle, children }) => {
+  const isKeyboardOpen = useKeyboardDetection();
+
   return (
     <div
       className={`fixed text-foreground top-0 left-0 w-full min-h-full bg-black bg-opacity-50 z-30  ${
         isOpen ? "block" : "hidden"
       }`}
-      onClick={toggle} 
+      onClick={toggle}
     >
       <div
-        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-popover rounded-xl  p-4 z-20 max-h-[90vh]  overflow-y-auto  border  hide-scrollbar sm:w-fit "
-        onClick={(e) => e.stopPropagation()} 
+        className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-popover rounded-xl  p-4 z-20 max-h-[90vh]  overflow-y-auto  border  hide-scrollbar sm:w-fit  ${
+          isKeyboardOpen ? "translate-y-[-50px]" : ""
+        } transition-transform duration-300`}
+        onClick={(e) => e.stopPropagation()}
       >
         {children}
       </div>
