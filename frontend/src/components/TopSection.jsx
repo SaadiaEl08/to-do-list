8b50605982143e1a2ddc69340909503ec1217b0f
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-01-27 23:43:14 +0100

    fix(TopSection): conditionally apply horizontal scaling to profile image based on source

--- frontend/src/components/TopSection.jsx
+++ frontend/src/components/TopSection.jsx
@@ -22,7 +22,7 @@ const TopSection = () => {
         <img
           src={image}
           alt="profile photo"
-          className="rounded-full w-11 h-11 object-fill -scale-x-100 border border-primary"
+          className={`rounded-full w-11 h-11 object-fill ${image.includes("ui-avatars.com") ? "" : "-scale-x-100"} border border-primary`}
         />
         
       </div>
