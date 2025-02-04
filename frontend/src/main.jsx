ca060a08d715dfe4d3b2a299051d082fb50d1930
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-05 00:03:03 +0100

    feat(query): integrate React Query by adding QueryClientProvider to main application

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import router from "./router";
 import { legacy_createStore } from "redux";
 import reducer from "./store/reducer";
@@ -9,13 +10,16 @@ import { Provider } from "react-redux";
 import { ThemeProvider } from "./contexts/ThemeContext";
 
 const store = legacy_createStore(reducer);
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <ThemeProvider>
-      <RouterProvider router={router}>
-        <App />
-      </RouterProvider>
-    </ThemeProvider>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <ThemeProvider>
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </ThemeProvider>
+    </Provider>
+  </QueryClientProvider>
 );
