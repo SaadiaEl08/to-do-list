1eab9f5492f60ab00afb75e9cce970f344eea262
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-05 00:03:12 +0100

    feat(auth): add custom hooks for user registration and login using React Query

---
+++ frontend/src/hooks/Auth.jsx
@@ -0,0 +1,25 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+const port = import.meta.env.VITE_BACKEND_PORT || 1337;
+const host = import.meta.env.VITE_BACKEND_HOST || "localhost";
+const API_URL = `http://${host}:${port}/api/auth/local`;
+
+// Register User
+export const useRegister = () => {
+  return useMutation(async (userData) => {
+    console.log(userData);
+    const { data } = await axios.post(`${API_URL}/register`, userData);
+    return data;
+  });
+};
+
+// Login User
+export const useLogin = () => {
+  return useMutation(async (credentials) => {
+    const { data } = await axios.post(`${API_URL}`, credentials);
+    localStorage.setItem("token", data.jwt);
+    localStorage.setItem("username", data.user.username);
+    return data;
+  });
+};
