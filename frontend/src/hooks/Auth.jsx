463bbff3338e7e0ded521f264502dcad89d58d5c
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-06 02:43:01 +0100

    feat(auth): enhance registration logic to handle dynamic email and username generation

--- frontend/src/hooks/Auth.jsx
+++ frontend/src/hooks/Auth.jsx
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
+import { useUpdateUser } from "./User";
 
 const port = import.meta.env.VITE_BACKEND_PORT || 1337;
 const host = import.meta.env.VITE_BACKEND_HOST || "localhost";
@@ -7,10 +8,36 @@ const API_URL = `http://${host}:${port}/api/auth/local`;
 
 // Register User
 export const useRegister = () => {
+  const { mutate } = useUpdateUser();
   return useMutation({
     mutationFn: async (userData) => {
-      const { data } = await axios.post(`${API_URL}/register`, userData);
-      return data;
+      //get all data from the form and send only password and email and username to the backend for registration
+      const { name, password, phoneNumber, registerMethod, login } = userData;
+      let email =
+        registerMethod === "email"
+          ? login
+          : `${new Date().getTime()}@gmail.com`;
+      let username =
+        registerMethod === "username"
+          ? login
+          : `${new Date().getTime()}`;
+
+      const { data } = await axios.post(`${API_URL}/register`, {
+        email,
+        username,
+        password,
+      });
+      localStorage.setItem("token", data.jwt);
+      //if the registration is successful, update the user with the new data 
+      if (data) {
+        const { jwt, user } = data;
+        mutate({
+          ...user,
+          phoneNumber,
+          name,
+          jwt,
+        });
+      }
     },
   });
 };
