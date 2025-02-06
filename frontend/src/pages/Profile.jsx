53c524b8f15ca3ab43465c6e6d076f2577b5c596
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-06 22:15:14 +0100

    feat(profile): integrate logout functionality and improve profile component structure

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -12,17 +12,17 @@ import {
   Settings,
   User,
 } from "lucide-react";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router";
+import { useState , useContext} from "react";
+import {  useNavigate } from "react-router";
 import AppInfo from "@/components/AppInfo";
 import ListItem from "@/components/ListItem";
 import { useSelector } from "react-redux";
+import { AuthContext } from "@/contexts/AuthContext";
 
 const Profile = () => {
   const [changeAccount, setChangeAccount] = useState(null);
   const [appInfos, setAppInfos] = useState(null);
-  const {name,image} = useSelector((state) => state.accountInfo);
-
+  const { name, image } = useSelector((state) => state.accountInfo);
   const nav = useNavigate();
   const listItems = [
     {
@@ -87,17 +87,20 @@ const Profile = () => {
   const completedTasks = useSelector(
     (state) => state.tasks.filter((task) => task.isCompleted).length
   );
+  const { logout } =useContext(AuthContext);
   return (
     <div className=" w-full text-foreground flex flex-col items-center gap-4 md:w-[86%]  ">
       <img
-        src={image} 
+        src={image}
         alt="profile photo"
-        className={`rounded-full w-20 h-20  object-fill ${image.includes("ui-avatars.com") ? "" : "-scale-x-100"}  border border-primary`}
+        className={`rounded-full w-20 h-20  object-fill ${
+          image.includes("ui-avatars.com") ? "" : "-scale-x-100"
+        }  border border-primary`}
       />
       <span className="capitalize">{name}</span>
       <div className=" w-full flex flex-wrap justify-evenly items-center px-4 py-2 gap-3">
         <div className="text-center px-9 py-4 bg-popover rounded-lg">
-          {notCompletedTasks} Task{notCompletedTasks > 1 ? "s" : ""}  left
+          {notCompletedTasks} Task{notCompletedTasks > 1 ? "s" : ""} left
         </div>
         <div className="text-center px-9 py-4 bg-popover rounded-lg">
           {completedTasks} Task{completedTasks > 1 ? "s" : ""} Done
@@ -109,13 +112,13 @@ const Profile = () => {
         ))}
       </div>
       <div className="w-full p-4 pb-6 flex  ">
-        <Link
-          to="/logout"
+        <button
+          onClick={logout}
           className="text-red-500  flex items-center justify-between "
         >
           <LogOut className="mr-2  w-6 h-6" />
           <span>Log out</span>
-        </Link>
+        </button>
       </div>
       {changeAccount === "name" && (
         <ChangeAccountName setChangeAccount={setChangeAccount} />
