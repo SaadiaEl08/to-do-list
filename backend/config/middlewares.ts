cb0b93f93057af4ac0ae7922132ed0a85432ee29
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-08 01:54:49 +0100

    feat(middlewares): update CORS configuration to allow all origins

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -6,11 +6,7 @@ export default ({ env }) => [
     config: {
       enabled: true,
       headers: "*",
-      origin: [
-        env("FRONTEND_URL", "http://localhost:5173"),
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-      ],
+      origin: ["*" ],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
       credentials: true,
     },
