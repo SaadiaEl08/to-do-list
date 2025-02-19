c66085db3fd03f1a307b8d7340a9a90deb7eaab6
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-19 15:29:47 +0100

    feat(task-form): enhance task creation and update logic with API integration and category handling

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,24 +1,81 @@
-import { steps } from "@/constants";
+import { useCategories } from "@/apis/Category";
+import { useCreateTask, useUpdateTask } from "@/apis/Task";
+import { myToast, steps } from "@/constants";
 import { AlarmClockPlus, LucideFlag, SendHorizonal, Tag } from "lucide-react";
-import { useEffect,useCallback } from "react";
+import { useEffect, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const TaskForm = () => {
   const dispatch = useDispatch();
   const mode = useSelector((state) => state.mode);
   const taskInfo = useSelector((state) => state.taskInfo);
-
+  const loginMode = useSelector((state) => state.loginMode);
+  const { data: categories = [] } = useCategories();
+  const { mutate: createTask } = useCreateTask();
+  const { mutate: updateTask } = useUpdateTask();
 
   const handleSaveTask = useCallback(() => {
+    // Check if title and description are provided
     if (taskInfo.title && taskInfo.description) {
-      mode === "create"
-        ? dispatch({ type: "CREATE_TASK", payload: taskInfo })
-        : dispatch({ type: "UPDATE_TASK", payload: taskInfo });
-      dispatch({ type: "SET_IS_OPEN_ADD_TASK", payload: false });
+      let { id, ...data } = taskInfo;
+      data.priority === "" && (data.priority = "null");
+      data.category == "" || data.category?.name === "null"
+        ? (data.category = categories.find((c) => c.name == "null")?.id)
+        : (data.category = categories.find((c) => c.name == data.category)?.id);
+      // Handle create mode
+      if (mode === "create") {
+        if (loginMode !== "fake-user") {
+          // If not fake-user, create the task via API
+          createTask(
+            { data: data },
+            {
+              onSuccess: () => {
+                // Dispatch the task to state
+                dispatch({ type: "CREATE_TASK", payload: taskInfo });
+                dispatch({ type: "SET_IS_OPEN_ADD_TASK", payload: false });
+              },
+              onError: (err) => {
+                // Log the error from API response
+                myToast(err.response.data.error.message, "error");
+              },
+            }
+          );
+        } else {
+          // If fake-user, create the task locally via dispatch
+          dispatch({ type: "CREATE_TASK", payload: taskInfo });
+          dispatch({ type: "SET_IS_OPEN_ADD_TASK", payload: false });
+        }
+      }
+      // Handle edit mode
+      else if (mode === "edit") {
+        if (loginMode !== "fake-user") {
+          // If not fake-user, update the task via API
+          updateTask(
+            { id: id, data: data },
+            {
+              onSuccess: () => {
+                // Dispatch the updated task to state
+                dispatch({ type: "UPDATE_TASK", payload: taskInfo });
+                dispatch({ type: "SET_IS_OPEN_ADD_TASK", payload: false });
+              },
+              onError: (err) => {
+                // Log the error from API response
+                myToast(err.response.data.error.message, "error");
+              },
+            }
+          );
+        } else {
+          // If fake-user, update the task locally via dispatch
+          dispatch({ type: "UPDATE_TASK", payload: taskInfo });
+          dispatch({ type: "SET_IS_OPEN_ADD_TASK", payload: false });
+        }
+      }
     } else {
+      // If title or description is missing, alert the user
       alert("Please fill all the fields");
     }
-  }, [dispatch, mode, taskInfo]);
+  }, [categories, createTask, dispatch, loginMode, mode, taskInfo, updateTask]);
+
   useEffect(() => {
     const enterClickEvent = (e) => {
       if (e.key === "Enter") {
@@ -32,9 +89,7 @@ const TaskForm = () => {
   }, [handleSaveTask]);
 
   return (
-    <div 
-    className={`w-[80vw] flex flex-col gap-4 sm:w-[50vw] lg:w-[40vw]`}
-    >
+    <div className={`w-[80vw] flex flex-col gap-4 sm:w-[50vw] lg:w-[40vw]`}>
       <h1>{mode === "create" ? "Add" : "Edit"} Task</h1>
       <input
         type="text"
@@ -82,7 +137,7 @@ const TaskForm = () => {
           />
           <LucideFlag
             className={`cursor-pointer ${
-              taskInfo.priority ? "text-primary" : ""
+              taskInfo.priority !== "" && taskInfo.priority !== "null" ? "text-primary" : ""
             }`}
             title="Set priority"
             onClick={() =>
