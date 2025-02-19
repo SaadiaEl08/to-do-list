447d924924c7bd5a50cb5a80867a9785cdd40808
SaadiaEL08 <saadiaelachguir@gmail.com>
2025-02-19 15:30:55 +0100

    feat(edit): enhance task retrieval logic to support different login modes and improve user feedback

--- frontend/src/pages/Edit.jsx
+++ frontend/src/pages/Edit.jsx
@@ -7,11 +7,12 @@ const Edit = () => {
   const id = useParams().taskId;
   const tasks = useSelector((state) => state.tasks);
   const [taskToShowDetail, setTaskToShowDetail] = useState(null);
+  const loginMode = useSelector((state) => state.loginMode);
   useEffect(() => {
-    const task = tasks.find((task) => task.id == id);
+    const task = tasks.find((task) => loginMode == "fake-user" ? task.id == id : task.documentId == id);
     setTaskToShowDetail(task);
-  }, [id, tasks]);
-  return taskToShowDetail && <TaskDetail task={taskToShowDetail} />;
+  }, [id, loginMode, tasks]);
+  return taskToShowDetail ? <TaskDetail task={taskToShowDetail} /> : <h1 className="text-center text-foreground">No tasks available under the given id</h1>;
 };
 
 export default Edit;
